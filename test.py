f7e56a168b070ba53524f05f87b1f94cb456b365
colin <colin2328@gmail.com>
2013-10-28 15:59:46 -0400

    added fbook app permissions

--- test.py
+++ test.py
@@ -1,6 +1,29 @@
 import facebook
 import re
 import datetime
+import hipchat
+import urllib
+import subprocess
+import urlparse
+
+
+fbook_secret = '1f8984df22ec832578f690f8b370d97c'
+fbook_id = '432841790154607'
+# Trying to get an access token. Very awkward.
+oauth_args = dict(client_id     = fbook_id,
+                  client_secret = fbook_secret,
+                  grant_type    = 'client_credentials')
+oauth_curl_cmd = ['curl',
+                  'https://graph.facebook.com/oauth/access_token?' + urllib.urlencode(oauth_args)]
+oauth_response = subprocess.Popen(oauth_curl_cmd,
+                                  stdout = subprocess.PIPE,
+                                  stderr = subprocess.PIPE).communicate()[0]
+
+try:
+    fbook_token = urlparse.parse_qs(str(oauth_response))['access_token'][0]
+except KeyError:
+    print('Unable to grab an access token!')
+    exit()
 
 def next_weekday(weekday_int):
 	# weekday_int: 0 = Monday, 1=Tuesday, 2=Wednesday...
@@ -19,14 +42,13 @@ def process_vendors(text):
 		split = result[0].split('\r\n')
 		return split[1:-3] #remove start string and end strings
 
-token = 'CAACEdEose0cBAHrRy2storpmKC56lKnAafEtGxvdS4HZArZAzOTHUrtZBqfC5ZBMp9KEpTYzBqT6pVfnR6upK6OgfdjP3ZCj5TQP6dL2v5igwQQ7O4kuZCfyyv4NBHLjIXPuM78XV0ZBAZCxW6kHbCmi9IFbbH7ig2EiOiRRUunZBIXTZCzp0bqQcVfgA9CNb7WYQRBZA4reSxMgAZDZD'
 address = '410 Minna St'
 event_name = "OffTheGridSF"
 
 next_wed = str(next_weekday(2)) 
 next_fri = str(next_weekday(4))
 
-graph = facebook.GraphAPI(token)
+graph = facebook.GraphAPI(fbook_token)
 off_grid_events = graph.get_connections(event_name, 'events')
 target_event_ids = []
 for event in off_grid_events['data']:
@@ -35,15 +57,27 @@ for event in off_grid_events['data']:
 
 
 events = graph.get_objects(cat='multiple', ids=target_event_ids, fields=['description', 'start_time', 'location'])
+
+hipchat_token = '35b7de36961929ed984c34bbfc0d08'
+hipchat_room_id = 320022
+hipchat_user = 'Food Fairy'
+hipster = hipchat.HipChat(token=hipchat_token)
+
 for id_no in events:
+	rtn_str = ''
 	event = events[id_no]
 	#assert that location and time really does match
 	assert(re.search(address, event['location']))
 	if re.search(next_wed, event['start_time']):
-		print 'Wednesday truck'
+		rtn_str += "Wednesday\'s trucks: "
 	elif re.search(next_fri, event['start_time']):
-		print 'Friday truck'
+		rtn_str += "Friday\'s trucks: "
 	lst = process_vendors( event['description'])
-	print lst
+	rtn_str += ', '.join(lst)
+	hipster.method('rooms/message', method='POST', parameters={'room_id': hipchat_room_id, 'from': hipchat_user, 'message': rtn_str})
+
+
+
+
 
 
